ff7841c3dd618fd76490b6c0a1b037c23ce3c0b1
Cornelis Gotschelm <gotschelm@gmail.com>
2013-08-07 07:25:57 +0200

    ipv6 support

--- netkitten.c
+++ netkitten.c
@@ -13,50 +13,60 @@
 	
 int main(int argc, char *argv[])
 {
- 	if ( argc != 3 ) {
+	if ( argc != 3 ) {
 		fprintf(stderr, "Usage: %s <ip> <port>\n", argv[0]);
 		exit(1);
 	}
 
-	int mysock;
+	int sfd;
 	struct sockaddr_in myaddr;
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
 	char buf[2048];
 	int n;
 
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_port = htons(atoi(argv[2]));
-	// set myaddr.sin_addr.s_addr
-	// see http://www.retran.com/beej/sockaddr_inman.html
-	//if ( inet_pton(AF_INET,argv[1], &myaddr.sin_addr) != 1 ){
-	//	fprintf(stderr, "Failed to set IP address\n");
-	//	exit(1);
-	//}
-	myaddr.sin_addr.s_addr = inet_addr(argv[1]);
-	mysock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	// pretty much stolen from man getaddrinfo
+	bzero(&hints, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_STREAM; 
 
-	if 
-	( connect(mysock, (struct sockaddr *)&myaddr, sizeof(myaddr)) == -1 ){
-		fprintf(stderr, "Failed to connect to %s %s\n", argv[1], 
-								argv[2]);
-		exit(1);
+	if (getaddrinfo(argv[1], argv[2], &hints, &result) != 0){
+		fprintf(stderr, "getaddrinfo failed");
 	}
 
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+			     rp->ai_protocol);
+		if (sfd == -1)
+			continue;
+		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+			break;                  /* Success */
+		close(sfd);
+	}	
+
+	if (rp == NULL) {               /* No address succeeded */
+		fprintf(stderr, "Could not connect\n");
+		exit(EXIT_FAILURE);
+	}
+
+	freeaddrinfo(result);           /* No longer needed */
+	//so far the manpage
+	
 	pid_t pid = fork();
 	if (pid == 0){ //Child thread to read from the net
-		while ( (n = read(mysock, buf, sizeof(buf))) > 0){
+		while ( (n = read(sfd, buf, sizeof(buf))) > 0){
 			if (VERBOSE)
 				fprintf(stderr,"\n[%d]>", n);
 			write(1, buf, n);
-                        fflush(stdout); // permanently: setbuf(stdout, NULL);
+			fflush(stdout); // permanently: setbuf(stdout, NULL);
 		}
 
 	}
 	else { //Parent thread to read from stdin(0)
 		while (	(n = read(0, buf, sizeof(buf))) > 0 ){
-			send(mysock, buf, n, 0);
- 		}
+			send(sfd, buf, n, 0);
+		}
 	}
 
-	close(mysock);
 	return 0;
 }
